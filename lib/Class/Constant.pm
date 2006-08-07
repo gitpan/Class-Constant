5b23d3119e6e8a83748adb62e74933f0e33f8a4c
Robert Norris <rob@eatenbyagrue.org>
2006-08-07 12:18:05 +0000

    Import of ROBN/Class-Constant-0.02 from CPAN.
    gitpan-cpan-distribution: Class-Constant
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         ROBN/Class-Constant-0.02.tar.gz
    gitpan-cpan-author:       ROBN
    gitpan-cpan-maturity:     released

--- lib/Class/Constant.pm
+++ lib/Class/Constant.pm
@@ -3,7 +3,7 @@ package Class::Constant;
 use warnings;
 use strict;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 my %ordinal_for_data;
 my %data_by_ordinal;
@@ -33,9 +33,16 @@ sub import {
             $data{ordinal} = $ordinal_for_data{$caller};
             $ordinal_for_data{$caller}++;
 
-            $data{object} = bless [ $caller, $data{ordinal} ], "Class::Constant::Value";
+            $data{object} = bless \do { my $x = $data{ordinal} }, $caller;
 
             $data{value} = $value;
+            $value++;
+
+            next;
+        }
+
+        if (ref $arg eq "HASH") {
+            $data{methods} = $value = $arg;
             $value++;
 
             next;
@@ -62,6 +69,9 @@ sub import {
     if ($start_ordinal == 0 and $ordinal_for_data{$caller} > 0) {
         do {
             no strict "refs";
+
+            unshift @{$caller."::ISA"}, "Class::Constant::Value";
+
             *{$caller."::by_ordinal"} = sub {
                 return if @_ < 2;
                 return $data_by_ordinal{$caller}->[$_[1]]->{object};
@@ -77,7 +87,7 @@ use Scalar::Util qw(refaddr);
 
 use overload
     q{""} => sub {
-        return "$data_by_ordinal{$_[0]->[0]}->[$_[0]->[1]]->{value}";
+        return "$data_by_ordinal{ref $_[0]}->[${$_[0]}]->{value}";
     },
     q{==} => sub {
         return ref $_[0] and ref $_[1] and refaddr $_[0] == refaddr $_[1] ? 1 : 0;
@@ -87,7 +97,26 @@ use overload
     };
 
 sub get_ordinal {
-    return $_[0]->[1];
+    return ${$_[0]};
+}
+
+sub AUTOLOAD {
+    my ($self) = @_;
+
+    use vars qw($AUTOLOAD);
+    my ($pkg, $method) = $AUTOLOAD =~ m/^(.*)::(.*)/;
+
+    return if $method =~ m/^[A-Z]+$/;
+
+    my $data = $data_by_ordinal{ref $_[0]}->[${$_[0]}];
+    return if not $data;
+
+    if (not exists $data->{methods} or not exists $data->{methods}->{$method}) {
+        use Carp qw(croak);
+        croak "Can't locate named constant \"$method\" for \"" .ref($_[0]). "::$data->{name}\"";
+    }
+
+    return $data->{methods}->{$method};
 }
 
 1;
@@ -99,6 +128,30 @@ __END__
 Class::Constant - Build constant classes
 
 =head1 SYNOPSIS
+
+    use Class::Constant NORTH, SOUTH, EAST, WEST;
+    
+    use Class::Constant
+        NORTH => "north",
+        SOUTH => "south",
+        EAST  => "east",
+        WEST  => "west;
+    
+    use Class::Constant
+        NORTH => { x =>  0, y => -1 },
+        SOUTH => { x =>  0, y =>  1 },
+        EAST  => { x => -1, y =>  0 },
+        WEST  => { x =>  1, y =>  0 };
+    
+    use Class::Constant
+        NORTH => "north",
+                 { x =>  0, y => -1 },
+        SOUTH => "south",
+                 { x =>  0, y =>  1 },
+        EAST  => "east",
+                 { x => -1, y =>  0 },
+        WEST  => "west",
+                 { x =>  1, y =>  0 };
 
 =head1 DESCRIPTION
 
@@ -152,6 +205,38 @@ for any given constant like so:
     ...
 
     print "You are facing $facing.\n";
+
+You can also associate named values with a constant by using a hashref:
+
+    use Class::Constant
+        NORTH => { x =>  0, y => -1 },
+        SOUTH => { x =>  0, y =>  1 },
+        EAST  => { x => -1, y =>  0 },
+        WEST  => { x =>  1, y =>  0 };
+
+    ...
+
+    print "About to move ".
+          $facing->x." in x and ".
+          $facing->y." in y.";
+
+And of course, you can do both:
+
+    use Class::Constant
+        NORTH => "north",
+                 { x =>  0, y => -1 },
+        SOUTH => "south",
+                 { x =>  0, y =>  1 },
+        EAST  => "east",
+                 { x => -1, y =>  0 },
+        WEST  => "west",
+                 { x =>  1, y =>  0 };
+
+    ...
+
+    print "Moving $facing will move you ".
+          $facing->x." in x and ".
+          $facing->y." in y.";
 
 =head1 AUTHOR
 
